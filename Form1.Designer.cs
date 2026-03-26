aae3e0da2d3d856f46e1b8ff2f5bd7791ae40f0c
강준서 <suwon030609@gmail.com>
2026-03-26 17:47:08 +0900

    과제4 코딩과 테스트 완료
    윈도우 계산기 스타일을 참고하여 과제4 기능 구현을 진행함
    숫자 입력 처리 구조를 정리하고 현재 입력값을 공통 메서드로 다룰 수 있도록 수정함
    연산자 표시를 ×, ÷ 형태로 출력하도록 화면 표시 로직을 개선함
    연산자 입력 직후에도 현재 값이 0으로 바뀌지 않고 기존 피연산자가 유지되도록 UpdateScreen 로직을 수정함
    소수점 입력 기능과 부호 반전 기능을 추가함
    키보드 입력 처리를 위한 KeyPreview 설정과 KeyPress, KeyDown 이벤트 연결 작업을 진행함
    계산 로직과 화면 갱신 로직을 정리하는 과정에서 중복 메서드 오류를 확인하고 수정 중임
    과제4 전체 완성 전까지 중간 구현 내용 저장

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -56,50 +56,51 @@
             // lblTitle
             // 
             lblTitle.AutoSize = true;
-            lblTitle.Font = new Font("Times New Roman", 31.875F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTitle.Font = new Font("Times New Roman", 37.875F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblTitle.ForeColor = SystemColors.GrayText;
-            lblTitle.Location = new Point(27, 38);
+            lblTitle.Location = new Point(12, 24);
             lblTitle.Name = "lblTitle";
-            lblTitle.Size = new Size(700, 96);
+            lblTitle.Size = new Size(832, 115);
             lblTitle.TabIndex = 0;
             lblTitle.Text = "Simple Calculator";
             // 
             // txtHistory
             // 
-            txtHistory.Font = new Font("맑은 고딕", 13.875F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            txtHistory.Font = new Font("맑은 고딕", 19.875F, FontStyle.Regular, GraphicsUnit.Point, 129);
             txtHistory.Location = new Point(27, 181);
             txtHistory.Name = "txtHistory";
             txtHistory.ReadOnly = true;
-            txtHistory.Size = new Size(722, 57);
+            txtHistory.Size = new Size(817, 78);
             txtHistory.TabIndex = 1;
             // 
             // txtDisplay
             // 
-            txtDisplay.Font = new Font("맑은 고딕", 19.875F, FontStyle.Regular, GraphicsUnit.Point, 129);
-            txtDisplay.Location = new Point(27, 273);
+            txtDisplay.Font = new Font("맑은 고딕", 25.875F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            txtDisplay.Location = new Point(27, 275);
             txtDisplay.Name = "txtDisplay";
             txtDisplay.ReadOnly = true;
-            txtDisplay.Size = new Size(722, 78);
+            txtDisplay.Size = new Size(817, 99);
             txtDisplay.TabIndex = 2;
             txtDisplay.Text = "0";
             // 
             // btnSign
             // 
             btnSign.Font = new Font("굴림", 15F, FontStyle.Bold);
-            btnSign.Location = new Point(27, 829);
+            btnSign.Location = new Point(27, 927);
             btnSign.Name = "btnSign";
-            btnSign.Size = new Size(150, 60);
+            btnSign.Size = new Size(180, 90);
             btnSign.TabIndex = 3;
             btnSign.Text = "+/-";
             btnSign.UseVisualStyleBackColor = true;
+            btnSign.Click += btnSign_Click;
             // 
             // btn0
             // 
             btn0.Font = new Font("굴림", 15F, FontStyle.Bold);
             btn0.ForeColor = SystemColors.Highlight;
-            btn0.Location = new Point(213, 829);
+            btn0.Location = new Point(246, 927);
             btn0.Name = "btn0";
-            btn0.Size = new Size(150, 60);
+            btn0.Size = new Size(180, 90);
             btn0.TabIndex = 4;
             btn0.Text = "0";
             btn0.UseVisualStyleBackColor = true;
@@ -108,20 +109,21 @@
             // btnDot
             // 
             btnDot.Font = new Font("굴림", 15F, FontStyle.Bold);
-            btnDot.Location = new Point(399, 829);
+            btnDot.Location = new Point(454, 927);
             btnDot.Name = "btnDot";
-            btnDot.Size = new Size(150, 60);
+            btnDot.Size = new Size(180, 90);
             btnDot.TabIndex = 5;
             btnDot.Text = ".";
             btnDot.UseVisualStyleBackColor = true;
+            btnDot.Click += btnDot_Click;
             // 
             // btnEqual
             // 
             btnEqual.Font = new Font("굴림", 15F, FontStyle.Bold);
             btnEqual.ForeColor = SystemColors.ControlText;
-            btnEqual.Location = new Point(599, 829);
+            btnEqual.Location = new Point(664, 927);
             btnEqual.Name = "btnEqual";
-            btnEqual.Size = new Size(150, 60);
+            btnEqual.Size = new Size(180, 90);
             btnEqual.TabIndex = 6;
             btnEqual.Text = "=";
             btnEqual.UseVisualStyleBackColor = true;
@@ -131,9 +133,9 @@
             // 
             btn1.Font = new Font("굴림", 15F, FontStyle.Bold);
             btn1.ForeColor = SystemColors.Highlight;
-            btn1.Location = new Point(27, 724);
+            btn1.Location = new Point(27, 814);
             btn1.Name = "btn1";
-            btn1.Size = new Size(150, 60);
+            btn1.Size = new Size(180, 90);
             btn1.TabIndex = 7;
             btn1.Text = "1";
             btn1.UseVisualStyleBackColor = true;
@@ -143,9 +145,9 @@
             // 
             btn2.Font = new Font("굴림", 15F, FontStyle.Bold);
             btn2.ForeColor = SystemColors.Highlight;
-            btn2.Location = new Point(213, 724);
+            btn2.Location = new Point(246, 814);
             btn2.Name = "btn2";
-            btn2.Size = new Size(150, 60);
+            btn2.Size = new Size(180, 90);
             btn2.TabIndex = 8;
             btn2.Text = "2";
             btn2.UseVisualStyleBackColor = true;
@@ -155,9 +157,9 @@
             // 
             btn3.Font = new Font("굴림", 15F, FontStyle.Bold);
             btn3.ForeColor = SystemColors.Highlight;
-            btn3.Location = new Point(399, 724);
+            btn3.Location = new Point(454, 814);
             btn3.Name = "btn3";
-            btn3.Size = new Size(150, 60);
+            btn3.Size = new Size(180, 90);
             btn3.TabIndex = 9;
             btn3.Text = "3";
             btn3.UseVisualStyleBackColor = true;
@@ -167,9 +169,9 @@
             // 
             btnPlus.Font = new Font("굴림", 15F, FontStyle.Bold);
             btnPlus.ForeColor = Color.Red;
-            btnPlus.Location = new Point(599, 724);
+            btnPlus.Location = new Point(664, 814);
             btnPlus.Name = "btnPlus";
-            btnPlus.Size = new Size(150, 60);
+            btnPlus.Size = new Size(180, 90);
             btnPlus.TabIndex = 10;
             btnPlus.Text = "+";
             btnPlus.UseVisualStyleBackColor = true;
@@ -179,9 +181,9 @@
             // 
             btn4.Font = new Font("굴림", 15F, FontStyle.Bold);
             btn4.ForeColor = SystemColors.Highlight;
-            btn4.Location = new Point(27, 629);
+            btn4.Location = new Point(27, 679);
             btn4.Name = "btn4";
-            btn4.Size = new Size(150, 60);
+            btn4.Size = new Size(180, 90);
             btn4.TabIndex = 11;
             btn4.Text = "4";
             btn4.UseVisualStyleBackColor = true;
@@ -191,9 +193,9 @@
             // 
             btn5.Font = new Font("굴림", 15F, FontStyle.Bold);
             btn5.ForeColor = SystemColors.Highlight;
-            btn5.Location = new Point(213, 629);
+            btn5.Location = new Point(246, 679);
             btn5.Name = "btn5";
-            btn5.Size = new Size(150, 60);
+            btn5.Size = new Size(180, 90);
             btn5.TabIndex = 12;
             btn5.Text = "5";
             btn5.UseVisualStyleBackColor = true;
@@ -203,9 +205,9 @@
             // 
             btn6.Font = new Font("굴림", 15F, FontStyle.Bold);
             btn6.ForeColor = SystemColors.Highlight;
-            btn6.Location = new Point(399, 629);
+            btn6.Location = new Point(454, 679);
             btn6.Name = "btn6";
-            btn6.Size = new Size(150, 60);
+            btn6.Size = new Size(180, 90);
             btn6.TabIndex = 13;
             btn6.Text = "6";
             btn6.UseVisualStyleBackColor = true;
@@ -215,9 +217,9 @@
             // 
             btnMinus.Font = new Font("굴림", 15F, FontStyle.Bold);
             btnMinus.ForeColor = Color.Red;
-            btnMinus.Location = new Point(599, 629);
+            btnMinus.Location = new Point(664, 679);
             btnMinus.Name = "btnMinus";
-            btnMinus.Size = new Size(150, 60);
+            btnMinus.Size = new Size(180, 90);
             btnMinus.TabIndex = 14;
             btnMinus.Text = "-";
             btnMinus.UseVisualStyleBackColor = true;
@@ -227,9 +229,9 @@
             // 
             btn7.Font = new Font("굴림", 15F, FontStyle.Bold);
             btn7.ForeColor = SystemColors.Highlight;
-            btn7.Location = new Point(27, 533);
+            btn7.Location = new Point(27, 539);
             btn7.Name = "btn7";
-            btn7.Size = new Size(150, 60);
+            btn7.Size = new Size(180, 90);
             btn7.TabIndex = 15;
             btn7.Text = "7";
             btn7.UseVisualStyleBackColor = true;
@@ -239,9 +241,9 @@
             // 
             btn8.Font = new Font("굴림", 15F, FontStyle.Bold);
             btn8.ForeColor = SystemColors.Highlight;
-            btn8.Location = new Point(213, 533);
+            btn8.Location = new Point(246, 539);
             btn8.Name = "btn8";
-            btn8.Size = new Size(150, 60);
+            btn8.Size = new Size(180, 90);
             btn8.TabIndex = 16;
             btn8.Text = "8";
             btn8.UseVisualStyleBackColor = true;
@@ -251,9 +253,9 @@
             // 
             btn9.Font = new Font("굴림", 15F, FontStyle.Bold);
             btn9.ForeColor = SystemColors.Highlight;
-            btn9.Location = new Point(399, 533);
+            btn9.Location = new Point(454, 539);
             btn9.Name = "btn9";
-            btn9.Size = new Size(150, 60);
+            btn9.Size = new Size(180, 90);
             btn9.TabIndex = 17;
             btn9.Text = "9";
             btn9.UseVisualStyleBackColor = true;
@@ -263,9 +265,9 @@
             // 
             btnMultiply.Font = new Font("굴림", 15F, FontStyle.Bold);
             btnMultiply.ForeColor = Color.Red;
-            btnMultiply.Location = new Point(599, 533);
+            btnMultiply.Location = new Point(664, 549);
             btnMultiply.Name = "btnMultiply";
-            btnMultiply.Size = new Size(150, 60);
+            btnMultiply.Size = new Size(180, 90);
             btnMultiply.TabIndex = 18;
             btnMultiply.Text = "×";
             btnMultiply.UseVisualStyleBackColor = true;
@@ -274,9 +276,9 @@
             // btnCE
             // 
             btnCE.Font = new Font("굴림", 15F, FontStyle.Bold);
-            btnCE.Location = new Point(27, 433);
+            btnCE.Location = new Point(27, 419);
             btnCE.Name = "btnCE";
-            btnCE.Size = new Size(150, 60);
+            btnCE.Size = new Size(180, 90);
             btnCE.TabIndex = 19;
             btnCE.Text = "CE";
             btnCE.UseVisualStyleBackColor = true;
@@ -285,9 +287,9 @@
             // btnC
             // 
             btnC.Font = new Font("굴림", 15F, FontStyle.Bold);
-            btnC.Location = new Point(213, 433);
+            btnC.Location = new Point(246, 419);
             btnC.Name = "btnC";
-            btnC.Size = new Size(150, 60);
+            btnC.Size = new Size(180, 90);
             btnC.TabIndex = 20;
             btnC.Text = "C";
             btnC.UseVisualStyleBackColor = true;
@@ -296,9 +298,9 @@
             // btnDel
             // 
             btnDel.Font = new Font("굴림", 15F, FontStyle.Bold);
-            btnDel.Location = new Point(399, 433);
+            btnDel.Location = new Point(454, 419);
             btnDel.Name = "btnDel";
-            btnDel.Size = new Size(150, 60);
+            btnDel.Size = new Size(180, 90);
             btnDel.TabIndex = 21;
             btnDel.Text = "del";
             btnDel.UseVisualStyleBackColor = true;
@@ -308,9 +310,9 @@
             // 
             btnDivide.Font = new Font("굴림", 15F, FontStyle.Bold);
             btnDivide.ForeColor = Color.Red;
-            btnDivide.Location = new Point(599, 433);
+            btnDivide.Location = new Point(664, 419);
             btnDivide.Name = "btnDivide";
-            btnDivide.Size = new Size(150, 60);
+            btnDivide.Size = new Size(180, 90);
             btnDivide.TabIndex = 22;
             btnDivide.Text = "÷";
             btnDivide.UseVisualStyleBackColor = true;
@@ -320,7 +322,7 @@
             // 
             AutoScaleDimensions = new SizeF(14F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(774, 929);
+            ClientSize = new Size(874, 1029);
             Controls.Add(btnDivide);
             Controls.Add(btnDel);
             Controls.Add(btnC);
@@ -346,8 +348,9 @@
             Controls.Add(lblTitle);
             KeyPreview = true;
             Name = "Form1";
-            Text = " ";
+            Text = " Calculator v1.0";
             KeyDown += Form1_KeyDown;
+            KeyPress += Form1_KeyPress;
             ResumeLayout(false);
             PerformLayout();
         }
--- Form1.cs
+++ Form1.cs
@@ -5,24 +5,18 @@ namespace SimpleCalculator
         private string firstInput = "";
         private string secondInput = "";
         private string currentOperator = "";
-
-        private string lastOperator = "";
-        private string lastSecondInput = "";
-        private bool isCalculated = false;
+        private bool isResultShown = false;
         public Form1()
         {
             InitializeComponent();
+
             txtHistory.Text = "";
             txtDisplay.Text = "0";
+
+            this.KeyPreview = true;
         }
         private void UpdateScreen()
         {
-            string displayOperator = currentOperator;
-
-            if (currentOperator == "*")
-                displayOperator = "×";
-            else if (currentOperator == "/")
-                displayOperator = "÷";
 
             if (currentOperator == "")
             {
@@ -31,29 +25,63 @@ namespace SimpleCalculator
             }
             else
             {
-                txtHistory.Text = $"{firstInput} {displayOperator} {secondInput}".Trim();
-                txtDisplay.Text = secondInput == "" ? firstInput : secondInput;
+                txtHistory.Text = $"{firstInput} {GetDisplayOperator(currentOperator)} {secondInput}".Trim();
+
+                if (secondInput == "")
+                    txtDisplay.Text = firstInput == "" ? "0" : firstInput;
+                else
+                    txtDisplay.Text = secondInput;
             }
 
-            if (txtDisplay.Text == "")
-                txtDisplay.Text = "0";
+        }
+        private string FormatNumber(double value)
+        {
+            return value.ToString("0.###############");
+        }
 
+        private string GetDisplayOperator(string op)
+        {
+            if (op == "*") return "×";
+            if (op == "/") return "÷";
+            return op;
+        }
+
+        private string GetCurrentInput()
+        {
+            return currentOperator == "" ? firstInput : secondInput;
+        }
+
+        private void SetCurrentInput(string value)
+        {
+            if (currentOperator == "")
+                firstInput = value;
+            else
+                secondInput = value;
+        }
+        private void AppendDigit(string digit)
+        {
+            if (isResultShown && currentOperator == "")
+            {
+                firstInput = "";
+                isResultShown = false;
+            }
+
+            string current = GetCurrentInput();
+
+            if (current == "0")
+                current = digit;
+            else if (current == "-0")
+                current = "-" + digit;
+            else
+                current += digit;
+
+            SetCurrentInput(current);
+            UpdateScreen();
         }
         private void NumberButton_Click(object sender, EventArgs e)
         {
             Button clickedButton = (Button)sender;
-            string digit = clickedButton.Text;
-
-            if (currentOperator == "")
-            {
-                firstInput += digit;
-            }
-            else
-            {
-                secondInput += digit;
-            }
-
-            UpdateScreen();
+            AppendDigit(clickedButton.Text);
         }
 
         private void btnPlus_Click(object sender, EventArgs e)
@@ -63,51 +91,30 @@ namespace SimpleCalculator
 
         private void btnEqual_Click(object sender, EventArgs e)
         {
-            if (firstInput == "" || secondInput == "" || currentOperator == "")
-                return;
-
-            double n1 = double.Parse(firstInput);
-            double n2 = double.Parse(secondInput);
-            double result = 0;
-
-            switch (currentOperator)
-            {
-                case "+":
-                    result = n1 + n2;
-                    break;
-                case "-":
-                    result = n1 - n2;
-                    break;
-                case "*":
-                    result = n1 * n2;
-                    break;
-                case "/":
-                    if (n2 == 0)
-                    {
-                        MessageBox.Show("0으로 나눌 수 없습니다.");
-                        return;
-                    }
-                    result = n1 / n2;
-                    break;
-            }
-
-            string displayOp = GetDisplayOperator();
-            txtHistory.Text = $"{firstInput} {displayOp} {secondInput} = {result}";
-            txtDisplay.Text = result.ToString();
-
-            firstInput = result.ToString();
-            secondInput = "";
-            currentOperator = "";
+            CalculateInternal();
         }
         private void SetOperator(string op)
         {
             if (firstInput == "")
                 return;
 
-            if (secondInput != "")
+            isResultShown = false;
+
+            if (currentOperator != "" && secondInput == "")
+            {
+                currentOperator = op;
+                UpdateScreen();
                 return;
+            }
+
+            if (currentOperator != "" && secondInput != "")
+            {
+                if (!CalculateInternal())
+                    return;
+            }
 
             currentOperator = op;
+            isResultShown = false;
             UpdateScreen();
         }
 
@@ -139,6 +146,7 @@ namespace SimpleCalculator
             firstInput = "";
             secondInput = "";
             currentOperator = "";
+            isResultShown = false;
 
             txtHistory.Text = "";
             txtDisplay.Text = "0";
@@ -146,33 +154,27 @@ namespace SimpleCalculator
         private void ClearEntry()
         {
             if (currentOperator == "")
-            {
                 firstInput = "";
-            }
             else
-            {
                 secondInput = "";
-            }
 
+            isResultShown = false;
             UpdateScreen();
         }
         private void DeleteLastChar()
         {
-            if (currentOperator == "")
-            {
-                if (firstInput.Length > 0)
-                {
-                    firstInput = firstInput.Substring(0, firstInput.Length - 1);
-                }
-            }
-            else
-            {
-                if (secondInput.Length > 0)
-                {
-                    secondInput = secondInput.Substring(0, secondInput.Length - 1);
-                }
-            }
+            string current = GetCurrentInput();
 
+            if (current.Length == 0)
+                return;
+
+            current = current.Substring(0, current.Length - 1);
+
+            if (current == "-")
+                current = "";
+
+            SetCurrentInput(current);
+            isResultShown = false;
             UpdateScreen();
         }
         private void btnCE_Click(object sender, EventArgs e)
@@ -189,14 +191,152 @@ namespace SimpleCalculator
         {
             DeleteLastChar();
         }
-        private string FormatNumber(double value)
-        {
-            return value.ToString("0.###############");
-        }
-       
+
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            
+            if (e.KeyCode == Keys.Enter)
+            {
+                CalculateInternal();
+                e.SuppressKeyPress = true;
+            }
+            else if (e.KeyCode == Keys.Back)
+            {
+                DeleteLastChar();
+                e.SuppressKeyPress = true;
+            }
+            else if (e.KeyCode == Keys.Escape)
+            {
+                ClearAll();
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar))
+            {
+                AppendDigit(e.KeyChar.ToString());
+                e.Handled = true;
+                return;
+            }
+
+            switch (e.KeyChar)
+            {
+                case '+':
+                    SetOperator("+");
+                    e.Handled = true;
+                    break;
+                case '-':
+                    SetOperator("-");
+                    e.Handled = true;
+                    break;
+                case '*':
+                case 'x':
+                case 'X':
+                    SetOperator("*");
+                    e.Handled = true;
+                    break;
+                case '/':
+                    SetOperator("/");
+                    e.Handled = true;
+                    break;
+                case '.':
+                    AppendDot();
+                    e.Handled = true;
+                    break;
+                case '=':
+                    CalculateInternal();
+                    e.Handled = true;
+                    break;
+            }
+        }
+        private void AppendDot()
+        {
+            if (isResultShown && currentOperator == "")
+            {
+                firstInput = "";
+                isResultShown = false;
+            }
+
+            string current = GetCurrentInput();
+
+            if (current == "")
+                current = "0";
+
+            if (!current.Contains("."))
+            {
+                current += ".";
+                SetCurrentInput(current);
+                UpdateScreen();
+            }
+        }
+
+        private void btnDot_Click(object sender, EventArgs e)
+        {
+            AppendDot();
+        }
+        private void ToggleSign()
+        {
+            string current = GetCurrentInput();
+
+            if (current == "" || current == "0")
+                return;
+
+            if (current.StartsWith("-"))
+                current = current.Substring(1);
+            else
+                current = "-" + current;
+
+            SetCurrentInput(current);
+            UpdateScreen();
+        }
+
+        private void btnSign_Click(object sender, EventArgs e)
+        {
+            ToggleSign();
+        }
+        private bool CalculateInternal()
+        {
+            if (firstInput == "" || secondInput == "" || currentOperator == "")
+                return false;
+
+            double n1 = double.Parse(firstInput);
+            double n2 = double.Parse(secondInput);
+            double result = 0;
+
+            switch (currentOperator)
+            {
+                case "+":
+                    result = n1 + n2;
+                    break;
+                case "-":
+                    result = n1 - n2;
+                    break;
+                case "*":
+                    result = n1 * n2;
+                    break;
+                case "/":
+                    if (n2 == 0)
+                    {
+                        MessageBox.Show("0으로 나눌 수 없습니다.");
+                        return false;
+                    }
+                    result = n1 / n2;
+                    break;
+            }
+
+            string resultText = FormatNumber(result);
+            string expression = $"{firstInput} {GetDisplayOperator(currentOperator)} {secondInput} = {resultText}";
+
+            txtHistory.Text = expression;
+            txtDisplay.Text = resultText;
+
+            firstInput = resultText;
+            secondInput = "";
+            currentOperator = "";
+            isResultShown = true;
+
+            return true;
         }
     }
 }
